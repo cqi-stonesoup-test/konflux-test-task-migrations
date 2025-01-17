c44813e98098c15f5486cfe58192ad92cd3bb57e
Chenxiong Qi <cqi@redhat.com>
2025-01-17 20:48:42 +0800

    Updates to prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,26 +2,16 @@
 
 set -euo pipefail
 
-: "${LIST_REPO_STATUS:=}"
-
-if [ -n "$LIST_REPO_STATUS" ]; then
-    cd "$HOME/code/konflux/mintmaker-renovate-image" || exit 1
-    echo "$HOME/code/konflux/mintmaker-renovate-image:  $(git branch --show-current)"
-    cd "$HOME/code/konflux/mintmaker" || exit 1
-    echo "$HOME/code/konflux/mintmaker:                 $(git branch --show-current)"
-    cd "$HOME/code/konflux/build-definitions" || exit 1
-    echo "$HOME/code/konflux/build-definitions:         $(git branch --show-current)"
-    cd "$HOME/code/konflux/infra-deployments" || exit 1
-    echo "$HOME/code/konflux/infra-deployments:         $(git branch --show-current)"
-    exit 0
-fi
-
 # Custom Mintmaker image and build-definitions fork are required for setting up a Konflux cluster.
 
 NOW=$(date --iso-8601=seconds | sed -e 's/:/_/g' -e 's/+/-/')
 declare -r NOW
 
-declare -r CONFIG_DATA_FILE=/tmp/konflux-infra-deployments-config-data.yaml
+declare -r CONFIG_DATA_FILE=config-data.yaml
+if [ -f "$CONFIG_DATA_FILE" ]; then
+    rm "$CONFIG_DATA_FILE"
+fi
+touch "$CONFIG_DATA_FILE"
 
 save_config() {
     local -r key=${1:?Missing key of the config.}
@@ -50,100 +40,87 @@ run() {
     "$@"
 }
 
-show_summary() {
-    echo "################### summary ###################"
-    echo
-    cat "$CONFIG_DATA_FILE"
-}
-
-trap 'show_summary' EXIT ERR
-
-
 ############## Build and push mintmaker-renovate image ##############
 
-: "${MINTMAKER_RENOVATE_IMAGE_REPO_DIR:="$HOME/code/konflux/mintmaker-renovate-image"}"
-declare -r MINTMAKER_RENOVATE_IMAGE_REPO_DIR
+: "${mintmaker_renovate_git_repo:="$HOME/code/konflux/mintmaker-renovate-image"}"
+declare -r mintmaker_renovate_git_repo
 
-echo "🔺 entering ${MINTMAKER_RENOVATE_IMAGE_REPO_DIR}"
-cd "$MINTMAKER_RENOVATE_IMAGE_REPO_DIR" || exit 1
+echo "🔺 entering ${mintmaker_renovate_git_repo}"
+cd "$mintmaker_renovate_git_repo" || exit 1
 ensure_on_a_topic_branch
 
-MINTMAKER_RENOVATE_REVISION=$(git rev-parse HEAD)
-declare -r MINTMAKER_RENOVATE_REVISION
+mintmaker_renovate_git_revision=$(git rev-parse HEAD)
+declare -r mintmaker_renovate_git_revision
 
-MINTMAKER_RENOVATE_IMAGE=quay.io/mytestworkload/mintmaker-renovate-image:${MINTMAKER_RENOVATE_REVISION}-${NOW}
-declare -r MINTMAKER_RENOVATE_IMAGE
+mintmaker_renovate_image=quay.io/mytestworkload/mintmaker-renovate-image:${mintmaker_renovate_git_revision}-${NOW}
+declare -r mintmaker_renovate_image
 
-save_config mintmaker-renovate-repo "${MINTMAKER_RENOVATE_IMAGE_REPO_DIR}"
+save_config mintmaker-renovate-repo "${mintmaker_renovate_git_repo}"
 save_config mintmaker-renovate-branch "$(git branch --show-current)"
-save_config mintmaker-renovate-revision "$MINTMAKER_RENOVATE_REVISION"
-save_config mintmaker-renovate-image "$MINTMAKER_RENOVATE_IMAGE"
+save_config mintmaker-renovate-revision "$mintmaker_renovate_git_revision"
+save_config mintmaker-renovate-image "$mintmaker_renovate_image"
 
-run podman build -t "$MINTMAKER_RENOVATE_IMAGE" .
-run podman push "$MINTMAKER_RENOVATE_IMAGE"
+run podman build -t "$mintmaker_renovate_image" .
+run podman push "$mintmaker_renovate_image"
 
 
 ############## Build and push mintmaker image ##############
 
-: "${MINTMAKER_IMAGE_REPO_DIR:="$HOME/code/konflux/mintmaker"}"
-declare -r MINTMAKER_IMAGE_REPO_DIR
+: "${mintmaker_git_repo:="$HOME/code/konflux/mintmaker"}"
+declare -r mintmaker_git_repo
 
-echo "🔺 entering ${MINTMAKER_IMAGE_REPO_DIR}"
-cd "$MINTMAKER_IMAGE_REPO_DIR" || exit 1
+echo "🔺 entering ${mintmaker_git_repo}"
+cd "$mintmaker_git_repo" || exit 1
 ensure_on_a_topic_branch
 
 # Update mintmaker-renovate-image into the manager Deployment
-sed -i "s|^\( \+mintmaker.appstudio.redhat.com/renovate-image\): .\+$|\1: ${MINTMAKER_RENOVATE_IMAGE}|" \
+sed -i "s|^\( \+mintmaker.appstudio.redhat.com/renovate-image\): .\+$|\1: ${mintmaker_renovate_image}|" \
     config/manager/manager.yaml
 
 git diff
 git add config/manager/manager.yaml
 git commit -m "Update custom mintmaker-renovate-image reference - ${NOW}"
 remote_url=$(git config list | awk -F= '/^remote.origin.url=/ {print $2}')
-declare -r remote_url
 echo "🔨 Push local changes to ${remote_url}"
 git push --force origin HEAD
+unset remote_url
 
-MINTMAKER_REVISION=$(git rev-parse HEAD)
-declare -r MINTMAKER_REVISION
+mintmaker_git_revision=$(git rev-parse HEAD)
+declare -r mintmaker_git_revision
 
-MINTMAKER_IMAGE=quay.io/mytestworkload/mintmaker:${MINTMAKER_REVISION}-${NOW}
-declare -r MINTMAKER_IMAGE
+mintmaker_image=quay.io/mytestworkload/mintmaker:${mintmaker_git_revision}-${NOW}
+declare -r mintmaker_image
 
-save_config mintmaker-repo "${MINTMAKER_IMAGE_REPO_DIR}"
+save_config mintmaker-repo "${mintmaker_git_repo}"
 save_config mintmaker-branch "$(git branch --show-current)"
-save_config mintmaker-revision "$MINTMAKER_REVISION"
-save_config mintmaker-image "$MINTMAKER_IMAGE"
+save_config mintmaker-revision "$mintmaker_git_revision"
+save_config mintmaker-image "$mintmaker_image"
 
-run podman build -t "$MINTMAKER_IMAGE" .
-run podman push "$MINTMAKER_IMAGE"
+run podman build -t "$mintmaker_image" .
+run podman push "$mintmaker_image"
 
 
 ############## Build and push tasks and pipelines from build-definitions fork ##############
 
 build_and_push_tasks_pipelines() {
-    : "${BUILD_DEFINITIONS_FORK:="$HOME/code/konflux/build-definitions"}"
-    declare -r BUILD_DEFINITIONS_FORK
+    : "${build_definitions_git_repo:="$HOME/code/konflux/build-definitions"}"
+    declare -r build_definitions_git_repo
 
-    echo "🔺 entering ${BUILD_DEFINITIONS_FORK}"
-    cd "$BUILD_DEFINITIONS_FORK" || exit 1
+    echo "🔺 entering ${build_definitions_git_repo}"
+    cd "$build_definitions_git_repo" || exit 1
     ensure_on_a_topic_branch
-
-    TASKS_TO_BUILD=$(yq '.spec.tasks[].name' pipelines/template-build/template-build.yaml)
-    declare -r TASKS_TO_BUILD
 
     echo "🔨 build and push tasks: $TASKS_TO_BUILD"
 
-    : "${QUAY_ORG:=mytestworkload}"
+    : "${quay_org:=mytestworkload}"
 
     # BUILD_TAG=  # Created automatically by the script
-    QUAY_NAMESPACE="$QUAY_ORG" \
+    QUAY_NAMESPACE="$quay_org" \
     SKIP_BUILD=1 \
     SKIP_INSTALL=1 \
-    TEST_TASKS="$TASKS_TO_BUILD" \
     ./hack/build-and-push.sh
 
-    save_config build-definitions-repo "${BUILD_DEFINITIONS_FORK}"
+    save_config build-definitions-repo "${build_definitions_git_repo}"
     save_config build-definitions-branch "$(git branch --show-current)"
 }
 
@@ -152,14 +129,14 @@ build_and_push_tasks_pipelines
 
 ############## Update infra-deployments ##############
 
-: "${INFRA_DEPLOYMENTS_REPO:="$HOME/code/konflux/infra-deployments"}"
-declare -r INFRA_DEPLOYMENTS_REPO
+: "${infra_deployment_git_repo:="$HOME/code/konflux/infra-deployments"}"
+declare -r infra_deployment_git_repo
 
-echo "🔺 entering ${INFRA_DEPLOYMENTS_REPO}"
-cd "$INFRA_DEPLOYMENTS_REPO" || exit 1
+echo "🔺 entering ${infra_deployment_git_repo}"
+cd "$infra_deployment_git_repo" || exit 1
 ensure_on_a_topic_branch
 
-save_config infra-deployments-repo "${INFRA_DEPLOYMENTS_REPO}"
+save_config infra-deployments-repo "${infra_deployment_git_repo}"
 save_config infra-deployments-branch "$(git branch --show-current)"
 
 # Lines added previously might remain there, just delete them.
@@ -168,22 +145,22 @@ echo "🔺 Configure MINTMAKER_* environment"
 # Save the original to show diff at the end of the script
 cp ./hack/preview.env ./hack/preview.env.orig
 
-sed -i "s|^\(export MINTMAKER_IMAGE_REPO\)=.*$|\1=${MINTMAKER_IMAGE%:*}|" ./hack/preview.env
-sed -i "s|^\(export MINTMAKER_IMAGE_TAG\)=.*$|\1=${MINTMAKER_IMAGE#*:}|" ./hack/preview.env
-sed -i "s|^\(export MINTMAKER_PR_SHA\)=.*$|\1=${MINTMAKER_REVISION}|" ./hack/preview.env
+sed -i "s|^\(export MINTMAKER_IMAGE_REPO\)=.*$|\1=${mintmaker_image%:*}|" ./hack/preview.env
+sed -i "s|^\(export MINTMAKER_IMAGE_TAG\)=.*$|\1=${mintmaker_image#*:}|" ./hack/preview.env
+sed -i "s|^\(export MINTMAKER_PR_SHA\)=.*$|\1=${mintmaker_git_revision}|" ./hack/preview.env
 
 echo "🔺 Update Mintmaker development layer"
 
 yq -i ".resources |= [
     \"../base\",
-    \"https://github.com/tkdchen/mintmaker/config/default?ref=${MINTMAKER_REVISION}\",
-    \"https://github.com/tkdchen/mintmaker/config/renovate?ref=${MINTMAKER_REVISION}\"
+    \"https://github.com/tkdchen/mintmaker/config/default?ref=${mintmaker_git_revision}\",
+    \"https://github.com/tkdchen/mintmaker/config/renovate?ref=${mintmaker_git_revision}\"
 ]" ./components/mintmaker/development/kustomization.yaml
 
 yq -i ".images |= [{
     \"name\": \"quay.io/konflux-ci/mintmaker\",
-    \"newName\": \"${MINTMAKER_IMAGE%:*}\",
-    \"newTag\": \"${MINTMAKER_IMAGE#*:}\"
+    \"newName\": \"${mintmaker_image%:*}\",
+    \"newTag\": \"${mintmaker_image#*:}\"
 }]" ./components/mintmaker/development/kustomization.yaml
 
 echo "🔺 Update Mintmaker staging layer"
@@ -191,36 +168,37 @@ echo "🔺 Update Mintmaker staging layer"
 yq -i ".resources |= [
     \"../../base\",
     \"../../base/external-secrets\",
-    \"https://github.com/tkdchen/mintmaker/config/default?ref=${MINTMAKER_REVISION}\",
-    \"https://github.com/tkdchen/mintmaker/config/renovate?ref=${MINTMAKER_REVISION}\"
+    \"https://github.com/tkdchen/mintmaker/config/default?ref=${mintmaker_git_revision}\",
+    \"https://github.com/tkdchen/mintmaker/config/renovate?ref=${mintmaker_git_revision}\"
 ]" \
 ./components/mintmaker/staging/base/kustomization.yaml
 
 yq -i ".images |= [
 {
     \"name\": \"quay.io/konflux-ci/mintmaker\",
-    \"newName\": \"${MINTMAKER_IMAGE%:*}\",
-    \"newTag\": \"${MINTMAKER_IMAGE#*:}\"
+    \"newName\": \"${mintmaker_image%:*}\",
+    \"newTag\": \"${mintmaker_image#*:}\"
 },
 {
     \"name\": \"quay.io/konflux-ci/mintmaker-renovate-image\",
-    \"newName\": \"${MINTMAKER_RENOVATE_IMAGE%:*}\",
-    \"newTag\": \"${MINTMAKER_RENOVATE_IMAGE#*:}\"
+    \"newName\": \"${mintmaker_renovate_image%:*}\",
+    \"newTag\": \"${mintmaker_renovate_image#*:}\"
 }
 ]" \
 ./components/mintmaker/staging/base/kustomization.yaml
 
 echo "🔺 Update build pipeline config"
-BUILD_PIPELINE_CONFIG_FILE=$(mktemp --suffix=-build-pipeline-config)
-trap 'rm $BUILD_PIPELINE_CONFIG_FILE' EXIT ERR
+build_pipeline_config_file=$(mktemp --suffix=-build-pipeline-config)
+declare -r build_pipeline_config_file
+trap 'rm $build_pipeline_config_file' EXIT ERR
 
 yq '.data."config.yaml"' \
     ./components/build-service/base/build-pipeline-config/build-pipeline-config.yaml \
-    >"$BUILD_PIPELINE_CONFIG_FILE"
+    >"$build_pipeline_config_file"
 
-BUNDLE_VALUES_ENV="${BUILD_DEFINITIONS_FORK}/bundle_values.env"
-if [ -f "$BUNDLE_VALUES_ENV" ]; then
-    source "$BUNDLE_VALUES_ENV"  # for accessing the CUSTOM_* pipeline bundles
+bundle_values_env="${build_definitions_git_repo}/bundle_values.env"
+if [ -f "$bundle_values_env" ]; then
+    source "$bundle_values_env"  # for accessing the CUSTOM_* pipeline bundles
 else
     echo "error: missing bundle_values.env created by build-and-push.sh" >&2
     exit 1
@@ -230,13 +208,13 @@ fi
 yq -i "
 (.pipelines[] | select(.name == \"docker-build-oci-ta\") | .bundle)
 |= \"${CUSTOM_DOCKER_BUILD_OCI_TA_PIPELINE_BUNDLE}\"
-" "$BUILD_PIPELINE_CONFIG_FILE"
+" "$build_pipeline_config_file"
 
-yq -i "(.data.\"config.yaml\") |= \"$(cat "$BUILD_PIPELINE_CONFIG_FILE")\"" \
+yq -i "(.data.\"config.yaml\") |= \"$(cat "$build_pipeline_config_file")\"" \
     ./components/build-service/base/build-pipeline-config/build-pipeline-config.yaml
 
 echo
-echo "⚒️ $INFRA_DEPLOYMENTS_REPO"
+echo "⚒️ $infra_deployment_git_repo"
 git status
 git add \
     components/mintmaker/development/kustomization.yaml \
